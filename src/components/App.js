5108baefa0471fa34419ee1734ce3ffaf58f8607
SYED RIYAZ <syedriyaz@192.168.1.10>
2023-03-04 23:26:36 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,20 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+  const [name, setName] = useState(" ");
+
+const onInputChange=(e)=>{
+    setName(e.target.value);
+
+};
+const onInputClick=(e)=>{
+ onInputChange(e.target.value);
+};
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={onInputChange}></input>
+      <button id='button' onClick={onInputClick}>Click</button>
+      <p id='text'> Hello my name is {name} and I study at Newton School</p>
     </div>
   )
 }
