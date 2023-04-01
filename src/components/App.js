badca7b7487ff1912cabb833d7e1b3de71025cf9
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-01 15:40:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
-//code here
-  const [name, setName] = useState(" ");
+  const [name, setName] = useState("");
 
-const onInputChange=(e)=>{
+  const onInputChange = (e) => {
     setName(e.target.value);
+  };
 
-};
-const onInputClick=(e)=>{
- onInputChange(e.target.value);
-};
+  const onInputClick = () => {
+    document.getElementById("text").innerHTML = `Hello my name is ${name} and I study at Newton School`;
+  };
+
   return (
     <div id="main">
-      <input id='input' onChange={onInputChange}></input>
-      <button id='button' onClick={onInputClick}>Click</button>
-      <p id='text'> Hello my name is {name} and I study at Newton School</p>
+      <input id="input" onChange={onInputChange}></input>
+      <button id="button" onClick={onInputClick}>
+        Click
+      </button>
+      <p id="text">Hello my name is {name} and I study at Newton School</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
